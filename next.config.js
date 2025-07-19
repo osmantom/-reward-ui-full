e741315db03cf367a9f529edaec27527c4f75865
osmantom <134515617+osmantom@users.noreply.github.com>
2025-07-19 18:01:00 +0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ['ar'],
+    defaultLocale: 'ar',
+    localeDetection: false
+  }
+}
+module.exports = nextConfig
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Cairo", "sans-serif"]
+      }
+    }
+  },
+  plugins: []
+}
